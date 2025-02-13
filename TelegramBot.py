7d38706067bc7226729ef3f9236e3561de80e649
JX <make231107@gmail.com>
2025-02-13 12:43:50 +0300

    13.02

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,14 +1,25 @@
 from Setings import Token
 import telebot
+from telebot import types
 
 token = Token
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+def strat(message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Зарегестрироваться 🔑")
+        btn2 = types.KeyboardButton("Изменить пароль⚙️")
+        btn3 = types.KeyboardButton("Помощь🌌")
+        markup.add(btn1,btn2,btn3)
+        
+
+
+
+
 
 
 bot.polling(none_stop=True, interval=0)
