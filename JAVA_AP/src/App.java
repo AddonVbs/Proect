0a29ed2cd931b3aae56826ebeb2105a81b5f7d97
JX <make231107@gmail.com>
2025-02-24 14:45:27 +0300

    jar

--- JAVA_AP/src/App.java
+++ JAVA_AP/src/App.java
@@ -1,6 +1,18 @@
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        try{
+
+            Class.forName("org.sqlite.JDBC");
+            Connection co  = DriverManager.getConnection(
+                "jdbc:sqlite:APP.db");
+            System.out.println("Connected");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
         
     }
 }
